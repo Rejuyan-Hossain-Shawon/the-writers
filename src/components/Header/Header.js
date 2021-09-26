478a3629f1d19f2286d43984b60bd57dc478ab3c
RejuyanHossainShawon <76831441+RejuyanHossainShawon@users.noreply.github.com>
2021-09-26 16:25:06 +0600

    headersection done

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2>The Writer Commitie</h2>
+            <p>A writer is a person who uses written words in different styles and techniques to communicate ideas. Writers produce different forms of literary art and creative writing such as novels, short stories, books, poetry, travelogues, plays, screenplays, teleplays, songs, and essays as well as other reports and news articles that may be of interest to the public</p>
+            <h2>Total Expenses: $100 Million</h2>
+        </div>
+    );
+};
+
+export default Header;
